b4fa8f9a9a53f3dba51f3e9c0c4bcbe132112c84
Thomas Kautenburger <tkautenb@its.jnj.com>
2021-06-27 21:26:08 +0200

    blockhound added

--- demo/src/main/java/de/legendlime/demo/DemoApplication.java
+++ demo/src/main/java/de/legendlime/demo/DemoApplication.java
@@ -3,10 +3,13 @@ package de.legendlime.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.blockhound.BlockHound;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		BlockHound.install(); // throws an exception when a blocking call is involved in REST API
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
--- demo/src/main/java/de/legendlime/demo/model/Department.java
+++ demo/src/main/java/de/legendlime/demo/model/Department.java
@@ -6,53 +6,60 @@ import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 /**
- * Department domain object. Persistable interface must be used
- * to tell the database if we make an INSERT or an UPDATE
+ * Department domain object. Persistable interface must be used to tell the
+ * database if we make an INSERT or an UPDATE
+ * 
  * @author Thomas Kautenburger
  *
  */
 @Table("department")
 public class Department implements Persistable<Long> {
-	
-    @Id
+
+	@Id
 	private Long deptId;
 	private String name;
 	private String description;
-		
-    @Transient
-    private boolean newDepartment;
+
+	@Transient
+	private boolean newDepartment;
 
 	@Override
 	@Transient
 	public Long getId() {
 		return this.getDeptId();
 	}
-    @Override
-    @Transient
-    public boolean isNew() {
-        return this.newDepartment || deptId == null;
-    }
 
-    public Department setAsNew() {
-        this.newDepartment = true;
-        return this;
-    }
+	@Override
+	@Transient
+	public boolean isNew() {
+		return this.newDepartment || deptId == null;
+	}
+
+	public Department setAsNew() {
+		this.newDepartment = true;
+		return this;
+	}
 
 	public Long getDeptId() {
 		return deptId;
 	}
+
 	public void setDeptId(long deptId) {
 		this.deptId = deptId;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
